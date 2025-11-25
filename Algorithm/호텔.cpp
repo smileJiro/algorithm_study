cf9e5237a3ad4852e72ee6b80cd5d8513050a6f6
smileJiro <xodnd1478@gmail.com>
2025-11-25 17:21:22 +0900

    문제: 호텔(BOJ 1106)
    설명:
    - 초기에 가성비를 구한 후 이분탐색을 사용했음
    - 문제에는 정수배 제약이 있어 해당 방식으로 최적해X
    - 1차원 dp 사용해 해결

--- Algorithm/호텔.cpp
+++ Algorithm/호텔.cpp
@@ -1,73 +1,40 @@
-//#pragma region 틀린 풀이 (가성비 계산 그리디 + 이분탐색) -> 반례 있어서 DP풀이가 정석
-//#include <bits/stdc++.h>
-//using namespace std;
-//using P = pair<int, int>; // cost, value
-//// 2초 제한 약 1억번
-//// 0 <= C < 1000, 0 <= N < 20 
-//// 시간복잡도는 널널하다. 
-//
-//
-//int C, N; // 목표 고객 수, 홍보할 수 있는 도시 수
-//P arr[22];
-//int main()
-//{
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//	cin >> C >> N;
-//	for (int i = 0; i < N; ++i)
-//	{
-//		int c, v;
-//		cin >> c >> v;
-//		arr[i].first = c;
-//		arr[i].second = v;
-//	}
-//
-//	// 이분탐색 + 그리디
-//	sort(arr, arr + N, [&](P& a, P& b)
-//		{
-//			float valueA = (float)a.second / (float)a.first;
-//			float valueB = (float)b.second / (float)b.first;
-//
-//			return valueA > valueB; // 내림차순
-//		});
-//
-//
-//	// m == 투자할 금액
-//	int s{ 0 }, e{ C };
-//	int m = (s + e) / 2;
-//	int answer = 1000;
-//	while (s <= e)
-//	{
-//		m = (s + e) / 2;
-//
-//		int curCost = m;
-//		int valueSum = 0;
-//		// 투자 대비 효과 계산
-//		for (int i = 0; i < N; ++i)
-//		{
-//			auto& [cost, value] = arr[i];
-//			int mul = curCost / cost;
-//			valueSum += curCost / cost * value;
-//			curCost -= cost * mul;
-//
-//			if (curCost == 0)
-//				break;
-//		}
-//
-//		// 인덱스 이동
-//		if (valueSum < C)
-//			s = m + 1;
-//		else
-//		{
-//			answer = min(answer, m);
-//			e = m - 1;
-//		}
-//	}
-//
-//	cout << answer << '\n';
-//	return 0;
-//}
-//
-//#pragma endregion
-//
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+using P = pair<int, int>; // cost, value
+int C, N;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> C >> N;
+	vector<P> cities(N);
+	// 도시 정보 입력
+	int max_cost = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> cities[i].first >> cities[i].second;
+		max_cost = max(max_cost, cities[i].first);
+	}
+		
+	int limit = max_cost * C;
+	vector<int> dp(limit + 1, 0);
+	dp[0] = 0;
+	for (int i = 1; i <= limit; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			auto& [cost, value] = cities[j];
+			if(i >= cost)
+				dp[i] = max(dp[i], dp[i - cost] + value);
+		}
+		
+		if (dp[i] >= C)
+		{
+			cout << i << '\n';
+			return 0;
+		}
+	}
+
+	return 0;
+}
